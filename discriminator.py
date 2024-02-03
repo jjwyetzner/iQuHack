9e421a9144a072a0bb9720eab57af245777207b6
tylerzchen <chent51605@gmail.com>
2024-02-03 13:32:31 -0500

    updated discriminator

--- discriminator.py
+++ discriminator.py
@@ -2,30 +2,31 @@
 import perceval as pcvl
 import perceval.components as comp
 import math
+import random
 from perceval.rendering.circuit import SymbSkin, PhysSkin
 import perceval
 
-phi = math.pi / 2
+phiList = [(random.random() * math.pi) for x in range(6)]
 theta = math.pi / 2
 
 discriminator = pcvl.Circuit(4)
 
-discriminator.add(0, comp.PS(phi))
-discriminator.add(1, comp.PS(phi))
-discriminator.add(2, comp.PS(phi))
+discriminator.add(0, comp.PS(phiList[0]))
+discriminator.add(1, comp.PS(phiList[1]))
+discriminator.add(2, comp.PS(phiList[2]))
 
 discriminator.add((0, 1), comp.BS(theta))
 discriminator.add((2, 3), comp.BS(theta))
 
-discriminator.add(0, comp.PS(phi))
-discriminator.add(2, comp.PS(phi))
+discriminator.add(0, comp.PS(phiList[3]))
+discriminator.add(2, comp.PS(phiList[4]))
 
 discriminator.add((0, 1), comp.BS(theta))
 discriminator.add((2, 3), comp.BS(theta))
 
 discriminator.add((1, 2), comp.BS(theta))
 
-discriminator.add(1, comp.PS(phi))
+discriminator.add(1, comp.PS(phiList[5]))
 
 discriminator.add((1, 2), comp.BS(theta))
 
